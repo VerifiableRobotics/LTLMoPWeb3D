b57c16c1be479bbe99d0774a37ec71d23174fd1d
Anton Gilgur <agilgur5@gmail.com>
2014-10-20 13:57:56 -0400

    indentation fix... python...

--- application.py
+++ application.py
@@ -190,4 +190,4 @@ def loadRegionEditor():
 if __name__ == '__main__':
 	#app.debug = True
 	port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port)
