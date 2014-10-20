8e383a92d183275f1ce3a9d9ea814f64a4d69eff
Anton Gilgur <agilgur5@gmail.com>
2014-10-20 13:50:45 -0400

    add procfile and port env variable

--- application.py
+++ application.py
@@ -188,5 +188,6 @@ def loadRegionEditor():
 	
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host='0.0.0.0')
+	#app.debug = True
+	port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
