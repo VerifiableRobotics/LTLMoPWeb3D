a75823312dbe498d486be3ef0999e1ef5449724e
Anton Gilgur <agilgur5@gmail.com>
2014-10-24 12:16:58 -0400

    add os chdir back to root

--- application.py
+++ application.py
@@ -191,5 +191,6 @@ if __name__ == '__main__':
 	#app.debug = True
 	os.chdir('LTLMoP/src/etc/jtlv')
 	os.system('sh build.sh')
+	os.chdir('../../../..')
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
