6bc590ebdd0b0ee029d28f66b1c10ead913bf9a8
Anton Gilgur <agilgur5@gmail.com>
2014-10-25 01:48:17 -0400

    setup.py unfortunately won't work like that (at least not with pip), so undo

--- application.py
+++ application.py
@@ -189,5 +189,10 @@ def loadRegionEditor():
 
 if __name__ == '__main__':
 	#app.debug = True
+  # compile jtlv compiler initially (build the java source with javac)
+  print "Compiling jtlv compiler..."
+  os.chdir('LTLMoP/src/etc/jtlv')
+  os.system('sh build.sh')
+  os.chdir('../../../..')
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
